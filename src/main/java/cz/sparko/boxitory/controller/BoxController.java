8450dded2d9baf1d21420616922e15cd89def946
Michal Vala <mvala@redhat.com>
2017-07-11 09:52:48 +0200

    unused import

--- src/main/java/cz/sparko/boxitory/controller/BoxController.java
+++ src/main/java/cz/sparko/boxitory/controller/BoxController.java
@@ -6,8 +6,6 @@ import cz.sparko.boxitory.service.BoxRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 public class BoxController {
     private BoxRepository boxRepository;
