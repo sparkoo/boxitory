fffded4a654e3f31c917685891e6db5c3316bf0e
Michal Vala <mvala@redhat.com>
2017-07-11 10:02:56 +0200

    merged IndexController into BoxController

--- src/main/java/cz/sparko/boxitory/controller/BoxController.java
+++ src/main/java/cz/sparko/boxitory/controller/BoxController.java
@@ -4,9 +4,11 @@ import cz.sparko.boxitory.conf.NotFoundException;
 import cz.sparko.boxitory.domain.Box;
 import cz.sparko.boxitory.service.BoxRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class BoxController {
     private BoxRepository boxRepository;
 
@@ -17,8 +19,14 @@ public class BoxController {
 
     @RequestMapping(value = "/{boxName}", method = RequestMethod.GET)
     @ResponseBody
-    public Box getBoxes(@PathVariable String boxName) {
+    public Box box(@PathVariable String boxName) {
         return boxRepository.getBox(boxName)
                 .orElseThrow(() -> new NotFoundException("box [" + boxName + "] does not exist"));
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("boxes", boxRepository.getBoxes());
+        return "index";
     }
 }
