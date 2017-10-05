4c9f98fb87c860f521a3459652539d4a90cffbb3
Michal Vala <michal.vala@gmail.com>
2017-10-05 15:05:21 +0200

    throw IllegalStateException when no repository directory found for both get box and boxes

--- src/main/java/cz/sparko/boxitory/conf/NotFoundException.java
+++ src/main/java/cz/sparko/boxitory/conf/NotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
-    private NotFoundException(String message) {
+    public NotFoundException(String message) {
         super(message);
     }
 
