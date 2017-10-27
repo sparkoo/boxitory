af643411cacb7a353994560eefdb046783a102c8
Michal Vala <michal.vala@gmail.com>
2017-10-27 10:12:44 +0200

    version sort e2e tests

---
+++ src/test/java/cz/sparko/boxitory/test/integration/VersionSortAscTest.java
@@ -0,0 +1,29 @@
+package cz.sparko.boxitory.test.integration;
+
+import org.springframework.test.context.TestPropertySource;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestPropertySource(properties = {"box.sort_desc=false"})
+public class VersionSortAscTest extends VersionSortTest {
+
+    @Test
+    public void givenSortAsc_whenGetBox_thenVersionsSortedAsc() throws Exception {
+        mockMvc.perform(get("/" + VM))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.versions[0].version", is("1")))
+                .andExpect(jsonPath("$.versions[1].version", is("2")))
+                .andExpect(jsonPath("$.versions[2].version", is("3")))
+                .andExpect(jsonPath("$.versions[3].version", is("5")))
+                .andExpect(jsonPath("$.versions[4].version", is("12")));
+    }
+}
---
+++ src/test/java/cz/sparko/boxitory/test/integration/VersionSortTest.java
@@ -0,0 +1,24 @@
+package cz.sparko.boxitory.test.integration;
+
+import java.io.File;
+import java.io.IOException;
+
+abstract public class VersionSortTest extends AbstractIntegrationTest {
+    final String VM = "vm";
+    final String VM_1_VBOX = VM + "_1_virtualbox.box";
+    final String VM_2_VBOX = VM + "_2_virtualbox.box";
+    final String VM_3_VBOX = VM + "_3_virtualbox.box";
+    final String VM_5_VBOX = VM + "_5_virtualbox.box";
+    final String VM_12_VBOX = VM + "_12_virtualbox.box";
+
+    @Override
+    void createFolderStructure() throws IOException {
+        createRepositoryDir();
+        File vmDir = createDirInRepository(VM);
+        createFile(vmDir.getPath() + File.separator + VM_5_VBOX);
+        createFile(vmDir.getPath() + File.separator + VM_12_VBOX);
+        createFile(vmDir.getPath() + File.separator + VM_3_VBOX);
+        createFile(vmDir.getPath() + File.separator + VM_1_VBOX);
+        createFile(vmDir.getPath() + File.separator + VM_2_VBOX);
+    }
+}
