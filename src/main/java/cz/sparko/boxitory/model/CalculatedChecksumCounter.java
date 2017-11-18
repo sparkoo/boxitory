076e54adf31f7eb8a013032d38a77a502ab34536
D3ryk <vaclav.telensky@seznam.cz>
2017-11-18 11:26:26 +0100

    CalculatedChecksumCounter unit test

--- src/main/java/cz/sparko/boxitory/model/CalculatedChecksumCounter.java
+++ src/main/java/cz/sparko/boxitory/model/CalculatedChecksumCounter.java
@@ -1,5 +1,7 @@
 package cz.sparko.boxitory.model;
 
+import java.util.Objects;
+
 public class CalculatedChecksumCounter {
     private final int maxCalculatedChecksum;
     private int alreadyEnsuredChecksum;
@@ -15,5 +17,27 @@ public class CalculatedChecksumCounter {
 
     public boolean isLimitOfCalculatedChecksumExceeded() {
         return alreadyEnsuredChecksum > maxCalculatedChecksum;
+    }
+
+    @Override
+    public String toString() {
+        return "CalculatedChecksumCounter{" +
+                "maxCalculatedChecksum=" + maxCalculatedChecksum +
+                ", alreadyEnsuredChecksum=" + alreadyEnsuredChecksum +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CalculatedChecksumCounter that = (CalculatedChecksumCounter) o;
+        return maxCalculatedChecksum == that.maxCalculatedChecksum &&
+                alreadyEnsuredChecksum == that.alreadyEnsuredChecksum;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(maxCalculatedChecksum, alreadyEnsuredChecksum);
     }
 }
