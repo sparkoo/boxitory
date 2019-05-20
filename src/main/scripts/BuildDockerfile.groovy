6e68071ce604b95d13c5fa0518b20dc790f71b10
Václav Telenský <vaclav.telensky@wandera.com>
2019-05-20 19:17:06 +0200

    Setting contianer the same path to host volume as container volume.

--- src/main/scripts/BuildDockerfile.groovy
+++ src/main/scripts/BuildDockerfile.groovy
@@ -1,7 +1,14 @@
+Properties props = new Properties()
+File propsFile = new File("${project.basedir}/src/main/resources/application.properties")
+props.load(propsFile.newDataInputStream())
+
 String template = new File("${project.basedir}/src/main/resources/DockerfileTemplate".toString()).getText()
 
 def dockerFileText = new groovy.text.SimpleTemplateEngine().createTemplate(template)
-        .make([fileName: project.build.finalName])
+        .make([
+                fileName: project.build.finalName,
+                workdir: props.getProperty("box.container.home")
+        ])
 
 println "writing dir " + "${project.basedir}/target/dockerfile"
 new File("${project.basedir}/target/dockerfile/".toString()).mkdirs()
